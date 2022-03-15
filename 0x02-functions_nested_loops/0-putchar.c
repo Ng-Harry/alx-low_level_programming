7fc257617118c6e692637df5b9621d5c2572856e
Ng-Harry <ngereharry5@gmail.com>
2022-03-15 13:34:20 +0100

    add code in putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,8 @@
+ 1   2   // A first program in C.
+ 3   #include <stdio.h>
+ 4
+ 5   // function main begins program execution
+ 6   int main( void )
+ 7   {
+ 8      printf( "Welcome to C!\n" );
+ 9   } // end function main
